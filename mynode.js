1f773a07457b26d306842272c72df8822e17f5d8
WEREstheWOLF <68578445+WEREstheWOLF@users.noreply.github.com>
2020-07-20 23:18:00 -0500

    Add files via upload

---
+++ mynode.js
@@ -0,0 +1,30 @@
+var http = require('http');
+
+var fs = require('fs');
+var htmlFile;
+var path = require('path');
+var url = require('url');
+var express = require('express');
+var app = express();
+
+//app.use('/css',express.styatic(__dirname + '/css'));
+//app.use('/public',express.static('public'));
+
+http.createServer(function (req, res){
+	var q = url.parse(req.url, true);
+	var filename = "." + q.pathname;
+	if(request.headers.appect.split(',')[0] == 'text/css'){
+		fs.readFile('index.css', (err, data)=>{
+			res.writeHeader(200, {'Content-Type': 'text/css'});
+			res.write(data);
+			res.end();
+	fs.readFile(filename, function(err, data){
+		if(err){
+			res.writeHead(404, {'Content-Type': 'text/html'});
+			return res.end("404 Not Found");
+		}
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		return res.end();
+	});
+}).listen(8080);
