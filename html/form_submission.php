b401e25c4bc5e24a091ce5c28eb7cf4bb21bd8ac
WEREstheWOLF <68578445+WEREstheWOLF@users.noreply.github.com>
2020-07-27 18:23:21 -0500

    Update form_submission.php

--- html/form_submission.php
+++ html/form_submission.php
@@ -1,1 +1,15 @@
-
+if((isset( $_POST['submit'])){
+  echo '<h2>form date retreived using $_REQUEST variable test<h2/>
+  $name = $_REQUEST['name'];
+  echo 'Your name is ' . $name;
+}
+if( filter_has_var(INPUT_POST, 'submit' )){
+  echo '<h2>form data retreived using $_POST variable test<h2/>'
+  $name = $_POST['name'];
+  echo 'Your name is " . $name;
+}
+if( filter_has_var(INPUT_GET, 'submit' )){
+  echo '<h2>form data retreived using $_GET variable test<h2/>'
+  $name = $_GET['name'];
+  echo 'Your name is " . $name;
+}
