fbd662c5d6c25b9f380b0430e139886eab77d61a
WEREstheWOLF <68578445+WEREstheWOLF@users.noreply.github.com>
2020-07-27 20:05:50 -0500

    Update form_submission.php

--- html/form_submission.php
+++ html/form_submission.php
@@ -1,7 +1,15 @@
 <?php
 $_POST['name'];
-  $name = $_POST['name'];
-  echo 'Your name is ' . $name;
+$name = $_POST['name'];
+echo 'Your name is ' . $name;
+if(isset($_POST['name'])){
+  $name=%_POST['name'];
+  $fp = fopen('data.txt', 'a');
+  fwrite($fp, $data);
+  fclose($fp);
+}
+?>
+
 
 /*if((isset( $_POST['submit'])){
   echo '<h2>form data retreived using $_REQUEST variable test</h2>'
@@ -18,4 +26,3 @@ if( filter_has_var(INPUT_GET, 'submit' )){
   $name = $_GET['name'];
   echo 'Your name is ' . $name;
 }*/
-?>
