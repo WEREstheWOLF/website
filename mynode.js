f5f72af20c02d90528d06ba4508ed631b03a0fdb
WEREstheWOLF <68578445+WEREstheWOLF@users.noreply.github.com>
2020-08-01 19:01:29 -0500

    Update mynode.js

--- mynode.js
+++ mynode.js
@@ -1,15 +1,9 @@
-var http = require('http');
-
 const express = require("express");
 const app = express();
 var bodyParser = require("body-parser");
-var mongoose = require("mongoose");
-var tj = require('templatesjs');
-var fs = require('fs');
-var nodemailer = require('nodemailer');
 const path = require('path')
+var nodemailer = require('nodemailer');
 
-var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 app.use(express.static('public'));
 app.use(express.static('public/css'));
@@ -18,170 +12,77 @@ app.use(express.static(__dirname));
 app.set('view engine', 'html');
 
 app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded());
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.heroku_nwkpq781);
-var feedbackSchema = new mongoose.Schema({
-	name: String,
-	address: String,
-	phone: String,
-	email: String,
-	comments: String
-});
-var feedback = mongoose.model("feedback", feedbackSchema);
+app.use(express.static('public'));
+app.use(express.static('public/css'));
+app.use(express.static(__dirname));
 
-app.post("/post-feedback", (req, res) =>{
-	function queryMongo(){
-		const userStatus = await feedback.findOneAndUpdate( {email: req.body.email}, {$set:{
-			name: req.body.name,
-			address: req.body.address,
-			phone: req.body.phone,
-			email: req.body.email,
-			comments: req.body.comments
-		}}, {upsert: true});
-		return userStatus;
-	}
+app.get('/', function(request, response) {
+  console.log('GET /')
+  var html = `
+    <html>
+        <body>
+            <form method="post" action="http://localhost:3000">Name: 
+                <input type="text" name="name" />
+                <input type="submit" value="Submit" />
+            </form>
+        </body>
+    </html>`
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.end(html)
+})
 
-	queryMongo().then(result => {
-		let userStatus = result;
-		let feedbackPage = fs.readFileSync(__dirname + 'html/contactMe.html');
-		
-		if (userStatus !== null) {
-			tj.setSync(feedbackPage);
-			let lineOne = "Thank you for revisiting, " + req.body.name + ">";
-			let lineTwo = "Your new feedback has been recorded.";
-			
-			var output = tj.renderSync("primarymessage",lineOne);
-			output = tj.setSync("secondarymessage",lineTwo);
-			
-			res.writeHead(200,{"Content-Type" : "text/html"});
-			res.write(output);
-			res.end();
-		}
-		else{
-			tj.setSync(feedbackPage);
-			let lineOne = "I apprecxiate your feedback, " + req.body.name + ".";
-			var output = tj.renderSync("primarymessage",lineOne);
-			feedback.count({}, function( err, count){
-				console.log(count);
-				lineTwo = "You are visitor number: <b>" + (count) + "</b>.";
-				output = tj.renderSync("secondarymessage",lineTwo);
-				res.writeHead(200,{"Content-Type" : "text/html"});
-				res.write(output);
-				res.end();
-			});
-		}
-	});
-	
-	let testAccount = await nodemailer.createTestAccount();
-	
-	let transporter = nodemailer.createTransport({
+app.post('/', function(request, response) {
+  console.log('POST /')
+  console.dir(request.body)
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  let lineOne = "Thank you for your response, " + request.body.name + ".<br>";
+  var output = lineOne;
+  let lineTwo = "Your address is: " + request.body.address + ".<br>";
+  output += lineTwo;
+  let lineThree = "Your Phone number is: " + request.body.phone + ".<br>";
+  output += lineThree;
+  let lineFour = "Your Email is: " + request.body.email + ".<br>";
+  output += lineFour;
+  let lineFive = "Your comments given are: " + request.body.comments + ".<br>";
+  output += lineFive;
+  
+  //start email
+  let transporter = nodemailer.createTransport({
 		host: "smtp.ethereal.email",
 		port: 587,
 		secure: false,
 		auth: {
-			user: testAccount.user,
-			pass: testAccount.pass,
+			user: 'testAccount.user',
+			pass: 'testAccount.pass',
 		}
 	});
-	
-	let info = await transporter.sendMail({
+  let info = transporter.sendMail({
 		from: 'noreplytyler38@gmail.com',
-		to: req.body.email,
+		to: request.body.email,
 		subject: '[EB]Submission Confirmation',
-		html: '<p>Dear ' + req.body.name + ' ' + ',</p><p>Thank you for your feedback submission.</p><p>Sincerely,</p><p>EB</p>
-	};
+		html: '<p>Dear ' + request.body.name + ' ' + ',</p><p>Thank you for your feedback submission.</p><p>Sincerely,</p><p>EB</p>'
+	});
 	
-	transporter.sendMail(mailOptions, function (err, info){
-		iff(err){
+	transporter.sendMail(function (err, info){
+		if(err){
 			console.log(err);
 		}
 		else{
 			console.log(info);
 		}
 	});
-	res.redirect('back');
-});
+	//response.redirect('back');
+  //end email
+  
+  response.write(output);
+  response.end('Thank you.')
+})
 
-/*app.post('/form_submission.php', function (req, res){
-	var body = '';
-	var testValidity = false;
-	req.on('data', function(chunk){
-		body += chunk.toString();
-	});
-	req.on('end', function(){
-		testValidity = ffv.validateForm(body); //check
-		if(testValidity === true){
-			var ts = Date.now();
-			var parsed = qs.parse(body);
-			fs.appendFile('data.txt', convertToString(parsed, ts)); 			//check
-			sendEmail(parsed['email'],ts);
-			res.writeHead(301, {'Content-Type': 'text/plain', Location: '/'} );
-			res.end();
-		}
-		else{
-			res.writeHead(301, {'Content-Type': 'text/plain', Location: '/'} );
-			res.end(testValidity);
-		}
-	});
-});		*/
-
-app.get('*', function(req, res){
-	if(req.url === '/favicon.ico'){
-		res.writeHead(200, {Content-Type': 'image/x-icon} );
-		return res.end();
-	}
-	var pathname = url.parse(req.url).pathname;
-	pathname = ( pathname === '/' || pathname === '' ) ? '/contactMe.html' : pathname;
-	var ext = path.extname(pathname);
-	fs.readFile(__dirname + pathname, function(err, data){
-		if(err){
-			if(ext){
-				res.writeHead(404, {'Content-Type': mimeTypes[ext]});
-			}
-			else{
-				res.writeHead(404, {'Content-Type': 'text/html'});
-			}
-			return res.end("404 Not Found");
-		}
-		if(ext){
-			res.writeHead(200, {'Content-Type': mimeTypes[ext]});
-		}
-		else{
-			res.writeHead(200, {'Content-Type': 'text/html'});
-		}
-		res.write(data);
-		return.end();
-	});
-});
-
-function convertToString(data, ts){
-	data.id = uuidvl();
-	data.created_at = Date();
-	data.reference_id = ts;
-	return JSON.stringify(data);
-}
-
-function sendEmail(email, reference){
-	var transporter = nodemailer.createTransport({
-		service: 'gmail',
-		host: 'smtp.gmail.com',
-		auth: {
-			user: noreplytyler38@gmail.com,
-			pass: abcdefghijkLMNOP123
-		}
-	});
-	var mailOptions = {
-		from: process.env.EMAILUSER,
-		to: email,
-		subject: 'Confirmation Email',
-		text: "Your information has been received.\nThank you for your feedback."
-	};
-	transporter.sendMail(mailOptions);
-}
-
+port = 3000
+app.listen(port)
+console.log(`Listening at http://localhost:${port}`)
 http.createServer(function (req, res){
 	var q = url.parse(req.url, true);
 	var filename = "." + q.pathname;
